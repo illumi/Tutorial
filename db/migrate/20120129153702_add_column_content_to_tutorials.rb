b11f61b68e98f0a246b48bd9a0e9fc7ae83b9d19
Jamie Spence <jamiespence2@gmail.com>
2012-01-29 15:42:31 +0000

    adding content column for tutorials

---
+++ db/migrate/20120129153702_add_column_content_to_tutorials.rb
@@ -0,0 +1,9 @@
+class AddColumnContentToTutorials < ActiveRecord::Migration
+  def self.up
+    add_column :tutorials, :content, :text
+  end
+  
+  def self.down
+    remove_column :tutorials, :content
+  end
+end
